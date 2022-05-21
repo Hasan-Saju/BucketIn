d399e4b346a67eab0520167fd1df1a2223e986c1
Hasan Saju <hasansaju11@gmail.com>
2022-05-21 15:35:30 +0600

    end3

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -55,7 +55,7 @@ const ProductScreen = (history) => {
                   text={`${product.numReviews} reviews`}
                 />
               </ListGroup.Item>
-              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+              <ListGroup.Item>Price: {product.price}/=</ListGroup.Item>
               <ListGroup.Item>
                 Description: {product.description}
               </ListGroup.Item>
